a103a1e75649964d001703cc6056a4d09a399d95
Renat0Ribeir0 <r.ribeiro@ext.arduino.cc>
2021-09-27 16:02:06 +0200

    tests

--- audio/client/script.js
+++ audio/client/script.js
@@ -1,5 +1,7 @@
 import {Remote} from "https://unpkg.com/@clinth/remote@latest/dist/index.mjs";
 import {getMinMaxAvg} from '../util.js';
+import {avg, scale} from '../util.js';
+
 
 const r = new Remote({
   ourId: 'ktppacj8ei13avongx8'
@@ -17,7 +19,28 @@ r.onData = (d) => {
   if (!freq) return;
 
   // Demo: Often useful to figure out the min/max/avg
-  const freqMMA = getMinMaxAvg(freq);
-  console.log(freqMMA);
+  const freqMMA = getMinMaxAvg(freq); 
+  //console.log(freqMMA.min);
+
+  var canvas = document.getElementById('myCanvas');
+  if (canvas.getContext) {
+    var ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = "white";
+    ctx.fillRect(0,0, canvas.width, canvas.height);
+
+    ctx.fillStyle = "black";
+    let W = freqMMA.min*-1;
+    let H = freqMMA.max*-1;
+    ctx.fillRect(50, 50,W, H);
+
+    console.log(W, H);
+  }
 }
+
+
+
+
+
+
 
